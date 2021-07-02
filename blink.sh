d561b768e4a7de07b88779b30b499716417b3845
Nick Cox <nickcox1008@gmail.com>
2021-07-02 21:35:54 +0100

    Add script

---
+++ blink.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <GPIO Pin Number> [sleep duration]"
+	echo
+	echo 'Optional sleep duration defaults to 1 second.'
+	echo
+	man sleep | head -n 15
+	exit
+fi
+
+PIN="$(printf '%.0f' $1)"
+
+if [ -n "$2" ]; then
+	SLEEP="${@:2}"
+else
+	SLEEP=1
+fi
+
+while true; do
+	gpio-toggle PIN 1
+	sleep SLEEP
+	gpio-toggle PIN 0
+	sleep SLEEP
+done
